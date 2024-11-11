90be3b747168197dd205d531d0943860a11a88c4
Samarth <167466628+samjam2005@users.noreply.github.com>
2024-11-12 03:53:58 +0800

    Update Medication.java
    added price attribute

--- OOP Semester Project/src/hms/Medication.java
+++ OOP Semester Project/src/hms/Medication.java
@@ -2,11 +2,14 @@ package hms;
 
 public class Medication {
     private String medicationName;
+    private int price;
     private int stockLevel;
     private int lowStockAlertLevel;
 
-    public Medication(String medicationName, int stockLevel, int lowStockAlertLevel) {
+
+    public Medication(String medicationName,int price, int stockLevel, int lowStockAlertLevel) {
         this.medicationName = medicationName;
+        this.price = price;
         this.stockLevel = stockLevel;
         this.lowStockAlertLevel = lowStockAlertLevel;
     }
@@ -17,6 +20,9 @@ public class Medication {
 
     public int getStockLevel() {
         return stockLevel;
+    }
+    public int getPrice() {
+        return price;
     }
 
     public int getLowStockAlertLevel() {
@@ -37,5 +43,14 @@ public class Medication {
         } else {
             System.out.println("Low stock alert level cannot be negative.");
         }
+    }
+
+    public int setPrice(int price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            System.out.println("Price cannot be negative.");
+        }
+        return price;
     }
 }
